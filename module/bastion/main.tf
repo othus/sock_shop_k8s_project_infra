de2af3c4ab56a1853d20ef6be1140ad33da22a2a
Raro <etobro@gmail.com>
2023-12-27 16:09:03 +0100

    bastion module infra code

---
+++ module/bastion/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "sockshop_Bastion" {
+  ami                         = var.ubuntu
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnets
+  vpc_security_group_ids      = [var.bastion-sg]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+  user_data                   = templatefile("../module/Bastion/bastion.sh", {
+    private-key               = var.private-key
+})
+  
+  tags = {
+    Name = var.bastion-name
+  }
+}
---
+++ module/bastion/varible.tf
@@ -0,0 +1,7 @@
+variable "ubuntu" {}
+variable "instance_type" {}
+variable "subnets" {}
+variable "bastion-sg" {}
+variable "key_name" {}
+variable "private-key" {}
+variable "bastion-name" {}
